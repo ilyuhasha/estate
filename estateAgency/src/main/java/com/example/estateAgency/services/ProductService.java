92d120f53ca11a7736fd8c8764c63588297a1135
ilyuhasha <134264080+ilyuhasha@users.noreply.github.com>
2023-05-22 10:35:49 +0300

    Add files via upload

---
+++ estateAgency/src/main/java/com/example/estateAgency/services/ProductService.java
@@ -0,0 +1,40 @@
+package com.example.estateAgency.services;
+
+import com.example.estateAgency.models.Product;
+import com.example.estateAgency.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+    public List<Product> listProducts(String title) {
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
+    }
+
+
+    public void saveProduct(Product product) {
+        log.info("Saving new {}", product);
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+
+}
+
+
+
